b962b35ec4c9e91eb823ba5cacd43e2e677e695a
Anto Fernandez <danisaantonella@gmail.com>
2026-08-04 18:16:06 -0300

    Singleton para configuración global: la dificultad afecta velocidad del cazador

---
+++ patterns/singleton/configuracion.py
@@ -0,0 +1,14 @@
+class Configuracion:
+    _instancia = None
+
+    def __new__(cls):
+        if cls._instancia is None:
+            cls._instancia = super().__new__(cls)
+            cls._instancia._inicializar()
+        return cls._instancia
+
+    def _inicializar(self):
+        # Estos valores solo se setean la primera vez que se crea la instancia
+        self.volumen_musica = 0.5
+        self.volumen_sonido = 0.7
+        self.dificultad ="facil"     # puede ser "facil", "normal", "dificil"
